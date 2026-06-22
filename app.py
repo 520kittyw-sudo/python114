eb4c943a60c7ea0de594e390dad6bc0ea4adf721
520kittyw-sudo <520kittyw@gmail.com>
2026-06-22 18:57:20 +0000

    Fix: Update templates path and structure

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
-from flask import Flask, render_template, request
-import numpy as np
+import os
+from flask import Flask, render_template
 
-app = Flask(__name__)
+# 這裡幫 Flask 指定絕對路徑
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
 
-# 確保這三個路徑是乾淨的
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/game')
-def game():
-    return render_template('game.html')
-
-@app.route('/result', methods=['POST'])
-def result():
-    # 這裡放你的計算邏輯
-    player_name = request.form.get('player_name')
-    # ... (其餘計算程式碼)
-    return render_template('result.html', name=player_name) 
-
-if __name__ == '__main__':
-    app.run()
